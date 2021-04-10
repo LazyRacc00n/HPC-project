f2096894fdde3574925040471cc1f1566b16eb50
LazyRacc00n <jacopo.dapueto@gmail.com>
2021-04-10 11:17:08 +0200

    starting envolve block

--- MPI/GoL_MPI/main_MPI.c
+++ MPI/GoL_MPI/main_MPI.c
@@ -11,112 +11,6 @@
 
 #include "gameOfLife_MPI.h"
 
-//------------------------------------ OLD CODE---------------------------------------------------------------------------------------
-
-void show(void *u, int w, int h)
-{
-	int x, y;
-	int(*univ)[w] = u;
-	printf("\033[H");
-	for (y = 0; y < h; y++)
-	{
-		for (x = 0; x < w; x++)
-			printf(univ[y][x] ? "\033[07m  \033[m" : "  ");
-		printf("\033[E");
-	}
-	fflush(stdout);
-	usleep(200000);
-}
-
-void printbig(void *u, int w, int h, int z)
-{
-	int x, y;
-	int(*univ)[w] = u;
-
-	FILE *f;
-
-	if (z == 0)
-		f = fopen("glife.txt", "w");
-	else
-		f = fopen("glife.txt", "a");
-
-	for (y = 0; y < h; y++)
-	{
-		for (x = 0; x < w; x++)
-			fprintf(f, "%c", univ[y][x] ? 'x' : ' ');
-		fprintf(f, "\n");
-	}
-	fprintf(f, "\n\n\n\n\n\n ******************************************************************************************** \n\n\n\n\n\n");
-	fflush(f);
-	fclose(f);
-}
-
-void evolve(void *u, int w, int h)
-{
-	unsigned(*univ)[w] = u;
-	unsigned new[h][w];
-	int x, y, x1, y1;
-
-	for (y = 0; y < h; y++)
-		for (x = 0; x < w; x++)
-		{
-			int n = 0;
-			for (y1 = y - 1; y1 <= y + 1; y1++)
-				for (x1 = x - 1; x1 <= x + 1; x1++)
-					if (univ[(y1 + h) % h][(x1 + w) % w])
-						n++;
-			if (univ[y][x])
-				n--;
-			new[y][x] = (n == 3 || (n == 2 && univ[y][x]));
-			/*
-		 * a cell is born, if it has exactly three neighbours 
-		 * a cell dies of loneliness, if it has less than two neighbours 
-		 * a cell dies of overcrowding, if it has more than three neighbours 
-		 * a cell survives to the next generation, if it does not die of loneliness 
-		 * or overcrowding 
-		 */
-		}
-	for (y = 0; y < h; y++)
-		for (x = 0; x < w; x++)
-			univ[y][x] = new[y][x];
-}
-
-void game(int w, int h, int t)
-{
-	int x, y, z;
-	unsigned univ[h][w];
-	struct timeval start, end;
-
-	//initialization
-	for (x = 0; x < w; x++)
-		for (y = 0; y < h; y++)
-			univ[y][x] = rand() < RAND_MAX / 10 ? 1 : 0;
-
-	if (x > 1000)
-		printbig(univ, w, h, 0);
-
-	for (z = 0; z < t; z++)
-	{
-		if (x <= 1000)
-			show(univ, w, h);
-		else
-			gettimeofday(&start, NULL);
-
-		evolve(univ, w, h);
-		if (x > 1000)
-		{
-			gettimeofday(&end, NULL);
-			printf("Iteration %d is : %ld ms\n", z,
-				   ((end.tv_sec * 1000000 + end.tv_usec) -
-					(start.tv_sec * 1000000 + start.tv_usec)) /
-					   1000);
-		}
-	}
-	if (x > 1000)
-		printbig(univ, w, h, 1);
-}
-
-//------------------------------------ NEW CODE    ---------------------------------------------------------------------------------------
 
 //function to allocate a block in a node and initialize the field of the struct it
 void init_and_allocate_block(struct grid_block *gridBlock, int nRows_with_ghost, int nCols_with_Ghost, int upper_neighbour, int lower_neighbour)
@@ -148,23 +42,32 @@ void init_grid_block(struct grid_block *gridBlock)
 
 //TODO: evolution of game of a block, and manage neighbours
 void evolve_block(struct grid_block *gridBlock,  int time)
-{
-	MPI_Datatype row;
+{	
+
+	//TODO: see difference of performance in seding using derived datatype and without
+	// create a derived datatyper to send a row
+	MPI_Datatype row_block_type;
+	MPI_Type_contiguous( gridBlock->numCols_ghost , MPI_UNSIGNED , &row_block_type);
+	MPI_Type_commit(&row_block_type);
+
+	MPI_Status stat;
+
 
 	for (t = 0; t < time; t++){
 
 		// send first row of the block to the upper neighbour
-
-		//TODO: see difference in seding using derived datatype and without derived datatype
-		MPI_Send( &gridBlock->block[1][0], gridBlock->numRows_ghost , MPI_Datatype datatype , int dest , int tag , MPI_Comm comm);
-
+		MPI_Send( &gridBlock->block[1][0], 1 , row_block_type , gridBlock->upper_neighbour, 0 , MPI_COMM_WORLD);
 		// send last row of the block to the lower neighbour
-		break;
-	
+		MPI_Send( &gridBlock->block[gridBlock->numRows_ghost - 2 ][0], 1 , row_block_type , gridBlock->lower_neighbour, 0 , MPI_COMM_WORLD);
+
+		// receive from below using  buffer the ghost row as receiver
+		MPI_Recv( &gridBlock->block[gridBlock->numRows_ghost - 1 ][0], gridBlock->numCols_ghost, MPI_UNSIGNED, gridBlock->lower_neighbour , 0 , MPI_COMM_WORLD, &stat);
+		
+		// receive from top using  the ghost row as receiver buffer
+		MPI_Recv( &gridBlock->block[1][0], gridBlock->numCols_ghost, MPI_UNSIGNED, gridBlock->uppper_neighbour , 0 , MPI_COMM_WORLD, &stat);
+
 	
 	}
-
-	return;
 }
 
 void game_block()
