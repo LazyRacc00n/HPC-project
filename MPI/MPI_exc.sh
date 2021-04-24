d384ab45a8e6f116638c7269c22eca17a6cd3583
LazyRacc00n <jacopo.dapueto@gmail.com>
2021-04-24 10:45:58 +0200

    MPI Script ended

--- MPI/MPI_exc.sh
+++ MPI/MPI_exc.sh
@@ -4,10 +4,19 @@ grid_dim_list=(100 500 1000 5000 10000 50000)
 
 display_versions_list=(1 2)
 
+not_show_evolution=1
+# path to file with the host list
+host_list="host_list.txt"
+
+bin_exec="../bin/gol_mpi"
+
 
 #Count number of nodes in the file host_list --> if the number is not available exit from the program
+# to change number of nodes, change file host_list.txt
 nodes="$(cat host_list.txt | wc -l)"
 
+
+#determanine automatically the list of the total number of processes
 case $nodes in
     
     1)
@@ -27,35 +36,25 @@ case $nodes in
         echo "NOT VALID NUMBER OF NODES!!"
         echo "Available number of nodes: 1 - 2 - 4 - 8"
         echo "Please chage the file host_list.txt"
+        echo "Or check if after last hostname there is an endline"
         printf "\n\n"
         exit 1
 esac
 
 
-for i in "${list_number_processes[@]}"
-do
-    echo $i
-done
-
-not_show_evolution=1
-
-# path to file with the host list
-host_list="host_list.txt"
-
-bin_exec="../bin/gol_mpi"
 
 for grid_dim in "${grid_dim_list[@]}"
 do
-   
+   printf "\n - #NODES: 1 - GRID DIM: $grid_dim x $grid_dim \n"
     for num_process in "${list_number_processes[@]}"
     do
         
         per_host=$(( num_process / nodes))
         
         for version in "${display_versions_list[@]}"
-        do
-
-            mpiexec -hostfile $host_list -perhost $per_host -np $num_precess ./$bin_exec $grid_dim $grid_dim 10 $version $not_show_evolution}
+        do  
+            
+            mpiexec -hostfile $host_list -perhost $per_host -np $num_process ./$bin_exec $grid_dim $grid_dim 10 $version $not_show_evolution
         
         done
     done
